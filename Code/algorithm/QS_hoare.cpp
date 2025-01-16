80ca990e6e353e1e9006544370c714b14ef81148
huydzai2 <166410099+huydzai2@users.noreply.github.com>
2025-01-16 10:33:36 +0700

    Update QS_hoare.cpp

--- Code/algorithm/QS_hoare.cpp
+++ Code/algorithm/QS_hoare.cpp
@@ -15,7 +15,7 @@ int heart(vector<int>& a,int l,int r){
       if(i>=j) { return j;}
       swap(a[i],a[j]);
        
-    }
+    }                                                                                                                                                
 }
 void quickSort(vector<int>& a,int l,int r){
     if(l<r){
@@ -23,11 +23,4 @@ void quickSort(vector<int>& a,int l,int r){
         quickSort(a,l,pi);
         quickSort(a,pi+1,r);
     }
-}
-int main(){
- vector<int> a={10, 7, 8, 9, 1, 5};
- quickSort(a,0,a.size()-1);
- for (int i=0;i<a.size();i++){
-    cout<<a[i]<<" ";
- }
 }
